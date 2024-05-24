a0b5a69fb82edbea36db946bd5a688dbf6b9f770
Artur Anikeich <65artur65@gmail.com>
2024-05-24 20:52:27 +0100

    3.22: Lint configuration

--- index.js
+++ index.js
@@ -5,127 +5,125 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
-//let persons = []
-
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
-morgan.token('body', function (req, res) { 
-    return req.method === "POST" ? JSON.stringify(req.body) : null
+morgan.token('body', function (req) {
+  return req.method === 'POST' ? JSON.stringify(req.body) : null
 })
 // with - at the end for non-POST, e. g. GET /api/persons/4 200 59 - 4.880 ms -
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 // without - at the end for non-POST, e.g. GET /api/persons/4 200 59 - 3.923 ms
 app.use(morgan(function (tokens, req, res) {
-    return [
-        tokens.method(req, res), " ",
-        tokens.url(req, res), " ",
-        tokens.status(req, res), " ",
-        tokens.res(req, res, 'content-length'), ' - ',
-        tokens['response-time'](req, res), ' ms ',
-        ...(req.method === "POST" ? tokens['body'](req, res) : [])
-    ].join('')
+  return [
+    tokens.method(req, res), ' ',
+    tokens.url(req, res), ' ',
+    tokens.status(req, res), ' ',
+    tokens.res(req, res, 'content-length'), ' - ',
+    tokens['response-time'](req, res), ' ms ',
+    ...(req.method === 'POST' ? tokens['body'](req, res) : [])
+  ].join('')
 }))
 
 const errorHandler = (error, request, response, next) => {
-    console.error("errorHandler", error.message)
-    console.error("error.name", error.name)
-  
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  console.error('errorHandler', error.message)
+  console.error('error.name', error.name)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
 }
 
 app.get('/api/persons', (request, response) => {
-    Person
-        .find({}).then(result => {
-            response.json(result)
-        })
+  Person
+    .find({}).then(result => {
+      response.json(result)
+    })
 })
 
 app.get('/info', (request, response) => {
-    Person
-        .find({}).then(result => {
-            response.send(`
-            <div>Phonebook has info for ${result.length} people.</div>
-            <br />
-            <div>${new Date().toString()}</div>
-            `)
-        })
+  Person
+    .find({}).then(result => {
+      response.send(`
+        <div>Phonebook has info for ${result.length} people.</div>
+        <br />
+        <div>${new Date().toString()}</div>
+        `)
+    })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Person.findById(id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error)) 
+  const id = request.params.id
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
+  const id = request.params.id
 
-    Person.findByIdAndDelete(id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndDelete(id)
+    .then(
+      response.status(204).end()
+    )
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    const body = request.body
+  const id = request.params.id
+  const body = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Person.findByIdAndUpdate(id, person, { new: true, runValidators: true })
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(id, person, { new: true, runValidators: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-  
-    if (!body.name) {
-        return response.status(400).json({ 
-            error: 'The name is missing' 
-        })
-    } else if (!body.number) {
-        return response.status(400).json({ 
-            error: 'The number is missing' 
-        })
-    }
-  
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'The name is missing'
     })
-    
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+  } else if (!body.number) {
+    return response.status(400).json({
+      error: 'The number is missing'
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const argsLength = process.argv.length
 
 if (argsLength < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -16,30 +16,30 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-if (argsLength == 3) {
-    console.log("phonebook:")
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+if (argsLength === 3) {
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
-} else if (argsLength == 5) {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4],
-    })
+    mongoose.connection.close()
+  })
+} else if (argsLength === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
 
-    person.save().then(result => {
-        console.log(`added ${result.name} number ${result.number} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(result => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
-    process.exit(1)
+  process.exit(1)
 }
